020fdf7408c2f9a9bd6bfce4ad0c742d623e3d14
derol78 <34183949+derol78@users.noreply.github.com>
2025-04-22 11:38:21 +0200

    updates

--- src/main/java/MqttSubscriber.java
+++ src/main/java/MqttSubscriber.java
@@ -1,17 +1,30 @@
 import org.eclipse.paho.client.mqttv3.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class MqttSubscriber {
     public static void main(String[] args) {
-        String broker = "tcp://localhost:1883"; // Replace with your broker's address.
-        String topic = "CIM_Analytics";
-        String clientId = "JavaClient";
-        String username = "test";
-        String password = "test";
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("src/main/resources/config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        String broker = properties.getProperty("broker", "tcp://localhost:1883");
+        String topic = properties.getProperty("topic", "CIM_Analytics");
+        String clientId = properties.getProperty("clientId", "JavaClient");
+        String username = properties.getProperty("username", "test");
+        String password = properties.getProperty("password", "test");
+        boolean cleanSession = Boolean.parseBoolean(properties.getProperty("cleanSession", "false"));
+        boolean debug = Boolean.parseBoolean(properties.getProperty("debug", "false"));
 
         try {
             MqttClient client = new MqttClient(broker, clientId);
             MqttConnectOptions options = new MqttConnectOptions();
-            options.setCleanSession(false);
+            options.setCleanSession(cleanSession);
             options.setUserName(username);
             options.setPassword(password.toCharArray());
 
@@ -23,7 +36,9 @@ public class MqttSubscriber {
 
                 @Override
                 public void messageArrived(String topic, MqttMessage message) throws Exception {
-                    System.out.println("Message received on topic " + topic + ": " + new String(message.getPayload()));
+                	if (debug) {
+                		System.out.println("Message received on topic " + topic + ": " + new String(message.getPayload()));
+                	}
                 }
 
                 @Override
@@ -34,7 +49,7 @@ public class MqttSubscriber {
 
             client.connect(options);
             System.out.println("Connected to broker: " + broker);
-            client.subscribe(topic,1);
+            client.subscribe(topic, 1);
             System.out.println("Subscribed to topic: " + topic);
 
         } catch (MqttException e) {
